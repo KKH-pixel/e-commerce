f8ab3fc19174b973f86dfd4cda43938c3b16deac
KKH-pixel <kendra.hunt@seattlecentral.edu>
2021-04-29 23:40:27 -0700

    uploading initial files for program

---
+++ routes/api/category-routes.js
@@ -0,0 +1,85 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    const allCategories = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(allCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try {
+    const allCategories = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!allCategories) {
+      res.status(404).json({ message: 'No reader found with that id!' });
+      return;
+    }
+
+    res.status(200).json(allCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+    try {
+      const allCategories = await Category.create(req.body);
+      res.status(200).json(allCategories);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  try {
+    const allCategories = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!allCategories[0]) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(allCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  try {
+    const allCategories = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!allCategories) {
+      res.status(404).json({ message: 'No reader found with that id!' });
+      return;
+    }
+
+    res.status(200).json(allCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
